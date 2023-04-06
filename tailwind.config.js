825a833e9be14da66029ca02e56a75cae422ccee
jhonjab19 <jhonjairob119g@gmail.com>
2023-04-06 11:11:53 -0500

    Add: Configuracion de fuente en tailwind clases universales

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./modules/**/*.{js,ts,jsx,tsx}"],
    theme: {
+      fontFamily: {
+         sans: ['"Nunito"', "sans-serif"]
+      },
       extend: {}
    },
    plugins: []
